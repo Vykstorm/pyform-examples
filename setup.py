67ef313c568ffc9def0dcd9c11eb7d192a3cd638
Vykstorm <victorruizgomezdev@gmail.com>
2018-11-07 15:28:39 +0100

    Changed setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ if __name__ == '__main__':
         author_email = 'victorruizgomezdev@gmail.com',
         python_requires = '>=2.7',
         install_requires = ['pyforms-gui', 'pyforms-web', 'pyforms-terminal'],
-        dependency_links = [''],
+        dependency_links = ['https://github.com/Vykstorm/pyforms-gui/tarball/master#egg=pyforms-gui'],
         keywords = ['gui', 'pyforms', 'examples']
     )
